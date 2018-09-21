56bbd546706926948e6f5cadb6635546df430d37
Anagha Rumade <rumadea@TDS-5S4-06K.fdny.org>
2018-09-21 17:33:36 -0400

    Few more queries using over and different order by giving different types of outputs

--- Over().sql
+++ Over().sql
@@ -9,7 +9,23 @@ select d.DEPT_NAME, sum(e.salary) over()
 from employee e, department d
 where e.dept_id = d.dept_id;
 
-select fname, lname, salary, e.dept_id, sum(salary) over(order by fname) running_sal, sum(salary) over(partition by d.dept_id order by e.fname) from employee e
+select fname, lname, salary, e.dept_id, sum(salary) over(order by fname) running_sal, sum(salary) over(partition by d.dept_id order by e.fname) 
+from employee e
 left join department d on e.dept_id = d.dept_id
 order by dept_id, fname
+;
+
+select lname, fname, dept_id, salary,
+sum(salary) over(order by salary) running_sal,
+SUM (salary)
+OVER (PARTITION BY dept_id ORDER BY salary) department_total
+from employee
+order by dept_id, salary, lname, fname
+;
+
+select lname, fname, dept_id, salary,
+SUM (salary)
+OVER (PARTITION BY dept_id ORDER BY salary) department_total
+from employee
+order by dept_id, lname, fname
 ;
