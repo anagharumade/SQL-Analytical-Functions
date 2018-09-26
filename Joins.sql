cb35fdb4f22431d85e2fb26ed97bc5823a07fe6b
Anagha Rumade <anagharumade.8@gmail.com>
2018-09-26 11:07:02 -0400

    Basic Joins in SQL

---
+++ Joins.sql
@@ -0,0 +1,43 @@
+/*
+
+Methods of performing Inner Join
+
+*/
+
+--Without using keyword "INNER JOIN"
+select * from employee e , department d where E.DEPT_ID = D.DEPT_ID;
+
+select * from employee e
+join department d on E.DEPT_ID = D.DEPT_ID;
+
+--Using keyword "INNER JOIN"
+select * from employee e
+inner join department d on E.DEPT_ID = D.DEPT_ID;
+
+--Inner join with a where clause
+select * from employee e
+inner join department d on E.DEPT_ID = D.DEPT_ID
+where e.dept_id = 10
+;
+
+--Left join
+select * from employee e
+left join department d on E.DEPT_ID = D.DEPT_ID
+;
+
+--Left join with a where clause
+select * from employee e
+left join department d on E.DEPT_ID = D.DEPT_ID
+where e.dept_id = 10
+;
+
+--Right join
+select * from employee e
+right join department d on E.DEPT_ID = D.DEPT_ID
+;
+
+--Right join with a where clause
+select * from employee e
+right join department d on E.DEPT_ID = D.DEPT_ID
+where e.dept_id = 10
+;
