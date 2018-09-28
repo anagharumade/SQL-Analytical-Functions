5e30b247b37a2f18d6a27cee4a28fda0d15ff798
Anagha Rumade <anagharumade.8@gmail.com>
2018-09-28 11:15:15 -0400

    Sum and avg functions

--- Aggregating_Fuctions.sql
+++ Aggregating_Fuctions.sql
@@ -6,4 +6,10 @@ group by D.DEPT_ID
 order by d.dept_id
 ;
 
---
+--Sum
+select d.dept_id, D.DEPT_NAME, count(e.emp_id) Count_of_Employees, sum(salary) Total_Department_Salary , avg(salary) average_department_salary
+from employee e, department d
+where e.dept_id = d.dept_id
+group by d.dept_id, D.DEPT_NAME
+order by d.dept_id, D.DEPT_NAME
+;
